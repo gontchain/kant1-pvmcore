5f6981c90d7ca0666ac23e6dda5501d76ba2bbc2
Alexander <samik.mechanic@gmail.com>
2018-09-10 15:28:43 +0300

    fix MSIZE op and test

--- tests/asm/msize/msize.asm
+++ tests/asm/msize/msize.asm
@@ -1,11 +1,16 @@
 {
-    0xffffffffffffffffffff
+    0x1234567890abcdef
+    0x01
     0x00
-    mstore
+    sub
+    0x20
+    mstore // store -1 in [0x20-0x40]
+    0x60
+    mstore // store 0x1234567890abcdef in [0x60-0x80]
     msize
     0x00
     mstore
-    0x02
+    0x01
     0x1f
-    return
+    return // 0x80 - biggest written byte address
 }
